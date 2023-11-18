1bdda5817d4c7fe3abee8d4b5e8e5d4ce7b1108a
haseeb <pkhaseeb04@gmail.com>
2023-11-18 10:51:12 -0800

    new

---
+++ script.mjs
@@ -0,0 +1,83 @@
+// console.log("Hello World");
+
+// let myname = "haseeb";
+// console.log(myname);
+
+// let age = "18";
+// console.log(age);
+
+// let weight = "88"
+// console.log(weight);
+
+// let qualification = "fsc"
+// console.log(qualification);
+
+// let organization = "Gcp"
+// console.log(organization);
+
+// let income = 0
+// console.log(income);
+
+// let gpa  = 3.6
+// console.log(gpa);
+
+// let brith_year = 2023 - age;
+// console.log(brith_year);
+
+// student_name = "haseeb"
+// teacher_name = "nasir"
+// employee_name = "hamid"
+// favouritebook = "javaScript"//without under scole
+// player_of_the_match = ""
+// PlayerOfTheMatch = ""//camal case
+
+// OPERATPOR
+//4+6 // 4 and 6 are operands + operator
+
+// alert("First Name : Muhammad ")
+// alert("Second Name : Haseeb")
+ //alert("Muhammad Haseeb")
+
+// let age = 45
+// console.log(age)
+// {
+//     let age = 60
+//     console.log(age)
+// }
+// console.log(age)
+
+//Operator
+//arithmetic operator : + - * / % **
+
+//   let brithyear =  Number(prompt("Enter your birth year"))
+//  console.log(typeof year)
+//  alert("your age is " +( 2023 - brithyear))
+
+
+let num = Number(prompt("enter a number"))
+
+if(num%2===0){
+    alert("Number is even")
+
+}
+else{
+    alert("number is odd")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
